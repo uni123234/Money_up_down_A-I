1a9d4db7be10a01929a405daa3d6a029551cfcab
uni123234 <uni2.0zxc@gmail.com>
2024-04-27 13:03:12 +0100

    Commit Start

---
+++ app/routs/rout.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.rout('/')
+def index():
+    '''
+    регістрація і логін і основна суть сайту ToDo list and Mony,up,down_list
+    '''
+    pass
+
+
+def login():
+    pass
+
+
+def register():
+    pass
