965f16f233e6221a3cc9ad4abe3e5433b34ba60d
uni123234 <uni2.0zxc@gmail.com>
2024-05-04 18:37:11 +0100

    .

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,9 +4,5 @@ import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {path:  'login', component: LoginComponent},
-<<<<<<< HEAD
     {path:  'home', component: HomeComponent},
-=======
-    {path:  '', component: HomeComponent},
->>>>>>> ab46e9143c5ab9b5228fa6981f8e6b522c59e209
 ];
