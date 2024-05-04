9b4deaf149271a4c2eb44c13bb87f32d79f3ce3e
uni123234 <uni2.0zxc@gmail.com>
2024-05-04 18:39:11 +0100

    .

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-<<<<<<< HEAD
 
 @Component({
   selector: 'app-root',
@@ -9,14 +8,6 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-=======
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
->>>>>>> ab46e9143c5ab9b5228fa6981f8e6b522c59e209
 })
 export class AppComponent {
   title = 'frontend';
