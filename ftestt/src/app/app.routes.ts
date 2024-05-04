294802915983261576c6b1ad02e39e3765f3ae96
Vano0928 <vanyapro57@gmail.com>
2024-05-04 13:29:51 +0300

    bag

--- ftestt/src/app/app.routes.ts
+++ ftestt/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path:  'login', component: LoginComponent},
-    {path:  '', component: HomeComponent},
+    {path: '', component: HomeComponent},
+    {path: 'login', component: LoginComponent}
 ];
