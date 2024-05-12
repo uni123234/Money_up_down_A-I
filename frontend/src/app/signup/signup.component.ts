cfdacf8d5469476b2b716349e154d02ebeaa5a17
Vano0928 <vanyapro57@gmail.com>
2024-05-12 14:34:56 +0300

    front login works

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,21 +1,48 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from '../data.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
 
-export class SignUp {
-  username: string;
+
+export class SignupComponent {
+
+
+  signupObj: SignupTemplate;
+
+  constructor(private dataService: DataService) {
+
+    this.signupObj = new SignupTemplate
+
+  }
+
+  signup() {
+    console.log(this.signupObj.email)
+    const loginData = { "email": this.signupObj.email, "password": this.signupObj.password };
+    this.dataService.login(loginData).subscribe({
+      next: (response) => console.log('Login successful', response),
+      error: (error) => console.error('Login failed', error)
+    });
+  }
+}
+
+
+export class SignupTemplate {
+
   email: string;
   password: string;
+  username: string;
 
   constructor() {
-    (this.username = ''), (this.email = ''), (this.password = '');
+    this.email = ""
+    this.password = ""
+    this.username = ""
   }
 }
