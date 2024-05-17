3d973f8a23684e24800fba2fc54524b18133c0e9
uni123234 <uni2.0zxc@gmail.com>
2024-05-17 07:56:42 +0200

    fix login

--- frontend/src/app/auth.guard.spec.ts
+++ frontend/src/app/auth.guard.spec.ts
@@ -1,17 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
-import { authGuard } from './auth.guard';
-
-describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+describe('AuthGuard', () => {
+  let authService: AuthService;
+  let authGuard: AuthGuard;
+  let router: Router;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [
+        AuthGuard,
+        { provide: AuthService, useValue: { isAuthenticated: jasmine.createSpy('isAuthenticated') } },
+        { provide: Router, useValue: jasmine.createSpyObj('Router', ['navigate']) }
+      ]
+    });
+
+    authService = TestBed.inject(AuthService);
+    authGuard = TestBed.inject(AuthGuard);
+    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    expect(authGuard).toBeTruthy();
+  });
+
+  it('should allow the authenticated user to access app', () => {
+    (authService.isAuthenticated as jasmine.Spy).and.returnValue(true);
+    expect(authGuard.canActivate({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot)).toBe(true);
+  });
+
+  it('should redirect an unauthenticated user to the login route', () => {
+    (authService.isAuthenticated as jasmine.Spy).and.returnValue(false);
+    expect(authGuard.canActivate({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot)).toBe(false);
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
   });
 });
